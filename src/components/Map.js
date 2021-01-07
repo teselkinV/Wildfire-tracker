a9e79fcd94441a4b65e97147eb78e530f0c7db74
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-01-07 09:38:42 -0800

    new project

---
+++ src/components/Map.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import GoogleMapReact from 'google-map-react';
+import LocationMarker from './LocationMarker';
+import LocationinfoBox from './LocationinfoBox';
+
+
+
+const Map = ({ eventData, center, zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+    
+
+    const markers = eventData.map(ev => {
+        if(ev.categories[0].id === 8) {
+            return <LocationMarker 
+                        lat={ev.geometries[0].coordinates[1]} 
+                        lng={ev.geometries[0].coordinates[0]} 
+                        onClick={() => setLocationInfo({ id: ev.id, title: ev.title})}
+                        
+                        />
+        }
+        return null;
+    })
+
+    return (
+        <div className="map">
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: 'AIzaSyCWuN5Uo4Lka4A6ac5jmxWK6ZvtgsTsSz0'
+                }}
+                defaultCenter={center}
+                defaultZoom={zoom}
+                >
+                    {markers}
+                </GoogleMapReact>
+            {locationInfo && <LocationinfoBox info={locationInfo}/>}
+        </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 42.3265,
+        lng: -122.8756
+    },
+    zoom: 6
+}
+
+export default Map
