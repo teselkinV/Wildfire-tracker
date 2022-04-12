5418cceb074a28191835cf1728886f8f14416b61
teselkinV <teselkinvova@gmail.com>
2022-04-12 11:15:23 -0700

    New info

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import Map from './components/Map';
-import Loader from './components/Loader';
-import Header from './components/Header';
+import React, { useState, useEffect } from "react";
+import Map from "./components/Map";
+import Loader from "./components/Loader";
+import Header from "./components/Header";
 
 function App() {
   const [eventData, setEventData] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const fetchEvents = async() => {
-      setLoading(true)
-      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
-      const { events } = await res.json()
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch(
+        "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+      );
+      const { events } = await res.json();
 
-      setEventData(events)
-      setLoading(false)
-    }
+      setEventData(events);
+      setLoading(false);
+      console.log("EVENTS", events);
+    };
 
-    fetchEvents()
-  }, [])
+    fetchEvents();
+  }, []);
 
   return (
     <div>
-      <Header/>
-      { !loading ? <Map eventData={eventData} /> : <Loader />}
+      <Header />
+      {!loading ? <Map eventData={eventData} /> : <Loader />}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,11 @@ const Header = () => {
         <header className='header'>
             <h1>California Fire Map & Tracker </h1> 
             <h2>Powered by <span className='nasa'>NASA</span></h2>
+            <p className='header-text'>An interactive map of wildfires burning across the Bay Area and California, 
+                including the Silverado Fire in Orange County and the Glass Fire in Napa.
+                <a className='link' href='https://www.sfchronicle.com/california-wildfires/article/fires-live-updates-Santa-Rosa-Napa-Glass-Shady-15495508.php'> 
+                Get breaking news and live updates including on evacuation orders and PG&E outages here.</a>
+                </p>
         </header>
     )
 }
